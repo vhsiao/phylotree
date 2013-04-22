aa51214cb35a173cfce3c5172811b3e5bcc87ef8
vhsiao <vhsiao@cs.brown.edu>
2013-04-22 06:41:17 +0000

    purged sensitive strings and set up custom config files

---
+++ _config.js
@@ -0,0 +1,14 @@
+// These credentials are placeholders.
+
+var config = {}
+
+config.mysql = {};
+config.redis = {};
+config.web = {};
+
+config.mysql.user_name = 'root'
+config.mysql.password = '';
+config.web.host = 'localhost';
+config.web.port = 8080;
+
+module.exports = config;
