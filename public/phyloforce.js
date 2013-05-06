b27baf66cb8b7900515e5d3dd3c694a7fbdce0bb
Vivian <vivian_hsiao@brown.edu>
2013-05-05 20:10:33 -0400

    gone infinity circles

--- public/phyloforce.js
+++ public/phyloforce.js
@@ -122,7 +122,7 @@ function visualize() {
     .attr("r", function(d){ 		
       //var r = ((2100-d.year)/320)*15;
       //var r = (1/(Math.floor(Math.log(2+d.group))))*20;
-      var r = 35-(Math.floor(Math.log(d.group)))*13;
+      var r = 35-(Math.floor(Math.log(1+d.group)))*13;
       return r;
     }) // ** set the radius of each circle
   .style("fill", function(d) {
